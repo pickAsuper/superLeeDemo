0d553899abc99ccc518e54fef80960ff9498b2ff
super <super123@126.com>
2017-04-08 10:02:36 +0800

    Initial Commit

---
+++ sinaDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sinaDemo
+//
+//  Created by admin on 17/4/8.
+//  Copyright © 2017年 super. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
